d5fbe8203da6206421a8b0f4a34692cdd47c96f4
Alston Pough <alstonpough@Alstons-MacBook-Pro.local>
2022-05-26 15:33:29 -0400

    finished

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
-// The following code is no longer needed in Grapql setup
-// const routes = require('./routes');
-// app.use(routes);
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -20,9 +17,7 @@ const server = new ApolloServer({
 });
 
 const app = express();
-// The parameter you provide to applyMiddleware is your middleware's top-level representation of your application. In Express applications, this variable is commonly named app.
-// When you pass your app to applyMiddleware, Apollo Server automatically configures various middleware (including body parsing, the GraphQL Playground frontend, and CORS support), so you don't need to apply them with a mechanism like app.use.
-server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -32,10 +27,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // (path.join(publicPath, 'index.html')); can do as well
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
 })
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 GraphQL is now listening on localhost:${PORT}${server.graphqlPath}`));
-});
+const runningServer =  async () => {
+  await server.start()
+    server.applyMiddleware({ app })
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 GraphQL is now listening on localhost:${PORT}${server.graphqlPath}`));
+  });
+}
+runningServer();
